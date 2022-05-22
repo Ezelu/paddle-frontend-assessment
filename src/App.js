d7e824a37a20ac96a1cb8c902da54f0277adfa64
ezelu <ezelujhay@gmail.com>
2022-05-22 15:26:36 -0700

    Fixed bugs and styling issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import Main from './components/Main';
 import './styles/App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Main from './pages/Main';
+
 
 
 export default function App () {
   return (
     <>
-      <Main />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/github' element={<Main />} />
+      </Routes>
     </>
   )
 }
